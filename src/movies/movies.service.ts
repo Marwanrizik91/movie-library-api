cf1dca2c3ba151a2dbfc57b31b505dfb3194348f
Marwan Rizik <59829281+Marwanrizik91@users.noreply.github.com>
2025-03-09 14:46:17 +0100

    ref: apply DRY principles

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,51 +1,71 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { AxiosResponse } from 'axios';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { GenresResponse, MoviesResponse } from './movies.interfaces';
 
 @Injectable()
 export class MoviesService {
-  private apiKey: string;
-  private readonly baseUrl: string = 'https://api.themoviedb.org/3';
+  private readonly apiKey: string;
+  private readonly baseUrl: string;
 
   constructor(
     private readonly httpService: HttpService,
     private readonly configService: ConfigService,
   ) {
-    this.apiKey = this.configService.get<string>('TMDB_API_KEY', '');
+    this.apiKey = this.configService.getOrThrow<string>('TMDB_API_KEY');
+    this.baseUrl = this.configService.getOrThrow<string>('TMDB_BASE_URL');
+  }
+
+  private buildUrl(
+    endpoint: string,
+    params: Record<string, string | number> = {},
+  ): string {
+    const queryParams = new URLSearchParams({
+      api_key: this.apiKey.toString(),
+      ...Object.entries(params).reduce(
+        (acc, [key, value]) => {
+          acc[key] = value.toString();
+          return acc;
+        },
+        {} as Record<string, string>,
+      ),
+    });
+    return `${this.baseUrl}${endpoint}?${queryParams.toString()}`;
   }
 
   getMovies(page: number = 1): Observable<MoviesResponse> {
+    const url = this.buildUrl('/discover/movie', { page });
     return this.httpService
-      .get(`${this.baseUrl}/discover/movie?api_key=${this.apiKey}&page=${page}`)
-      .pipe(map((response: AxiosResponse<MoviesResponse>) => response.data));
+      .get<MoviesResponse>(url)
+      .pipe(map((response) => response.data));
   }
 
   searchMovies(query: string, page: number = 1): Observable<MoviesResponse> {
+    const url = this.buildUrl(`/search/movie`, { query, page });
     return this.httpService
-      .get(
-        `${this.baseUrl}/search/movie?api_key=${this.apiKey}&query=${query}&page=${page}`,
-      )
-      .pipe(map((response: AxiosResponse<MoviesResponse>) => response.data));
+      .get<MoviesResponse>(url)
+      .pipe(map((response) => response.data));
   }
 
   getGenres(): Observable<GenresResponse> {
+    const url = this.buildUrl(`/genre/movie/list`);
     return this.httpService
-      .get(`${this.baseUrl}/genre/movie/list?api_key=${this.apiKey}`)
-      .pipe(map((response: AxiosResponse<GenresResponse>) => response.data));
+      .get<GenresResponse>(url)
+      .pipe(map((response) => response.data));
   }
 
   getMoviesByGenre(
     genreId: number,
     page: number = 1,
   ): Observable<MoviesResponse> {
+    const url = this.buildUrl(`/discover/movie`, {
+      with_genres: genreId,
+      page,
+    });
     return this.httpService
-      .get(
-        `${this.baseUrl}/discover/movie?api_key=${this.apiKey}&with_genres=${genreId}&page=${page}`,
-      )
-      .pipe(map((response: AxiosResponse<MoviesResponse>) => response.data));
+      .get<MoviesResponse>(url)
+      .pipe(map((response) => response.data));
   }
 }
