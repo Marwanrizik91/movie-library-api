a6149e4150cb20e1321cc146a3a1df3ff10dedfb
Marwan Rizik <59829281+Marwanrizik91@users.noreply.github.com>
2025-03-20 18:47:32 +0100

    feat: configure CORS and add Dockerfile for deployment
    Enable CORS with specific origin, methods, and headers to allow cross-origin requests. Add Dockerfile to containerize the NestJS application for deployment.

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { MoviesModule } from './movies/movies.module';
 async function bootstrap() {
   const app = await NestFactory.create(MoviesModule);
   app.setGlobalPrefix('api'); // Add the 'api' prefix
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
