b6b677fd3776e901d1e9889e8c960f6334dc2406
Marwan Rizik <59829281+Marwanrizik91@users.noreply.github.com>
2025-03-20 14:58:06 +0100

    Revert: main.ts cors settings

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { MoviesModule } from './movies/movies.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MoviesModule, { cors: true });
+  const app = await NestFactory.create(MoviesModule);
+  app.enableCors();
   app.setGlobalPrefix('api'); // Add the 'api' prefix
   await app.listen(process.env.PORT ?? 3000);
 }
